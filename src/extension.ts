10374bccc08796870a109ccca1a03a4e64126462
Alex Krechik <alexander.krechick@revjet.com>
2016-12-06 19:16:51 +0200

    0.1.0 version of plugin

--- src/extension.ts
+++ src/extension.ts
@@ -1,29 +1,64 @@
 'use strict';
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
+import {ExtensionContext, commands, window, Position, Range} from 'vscode';
 
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "VSVarsInserter" is now active!');
-
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with  registerCommand
-    // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('extension.sayHello', () => {
-        // The code you place here will be executed every time your command is executed
-
-        // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World!');
+export function activate(context: ExtensionContext) {
+    let disposable = commands.registerCommand('extension.insertVars', () => {
+        (new VarsInserter).insertVars();
     });
-
     context.subscriptions.push(disposable);
 }
 
-// this method is called when your extension is deactivated
 export function deactivate() {
+}
+
+class VarsInserter {
+
+    public insertVars() {
+        let vars = this.__getReturnVars();
+        let returnBlock = this.__getNewReturnBlock(vars);
+        let range = this.__getReturnBlockRange();
+        window.activeTextEditor.edit((edit) => {
+            edit.replace(range, returnBlock + '\n};');
+        });
+    }
+
+    private __getNewReturnBlock(vars: String[]) {
+        let block = ['\treturn {'];
+        vars.forEach((v) => {
+            if (v === '') {
+                block.push('');
+            } else {
+                block.push('\t\t' + v + ' : ' + v + ',');
+            }
+        })
+        block.push('\t}');
+        return block.join('\n');
+    }
+
+    private __getReturnBlockRange() {
+        var text = window.activeTextEditor.document.getText().split(/\n/);
+        var startLine = text.findIndex((el) => {return el.match(/return.*\{/) !== null});
+        var startChar = 0;
+        var startPosition = new Position(startLine, startChar);
+        var lastLine = text.length;
+        var lastChar = text[text.length - 1].length;
+        var lastPosition = new Position(lastLine, lastChar);
+        return new Range(startPosition, lastPosition);
+    }
+
+    private __getReturnVars() {
+        let returnVars = [];
+        let text = window.activeTextEditor.document.getText().split(/[\s\n]*module.exports.*\{/)[1];
+        text.split(/\n/).forEach((line) => {
+            if((line.match('var') || line.match('let')) && !(line.match(/^\s*\/\/.*/))) {
+                let variable = line.replace(/.*(var|let)\s*/, '').match(/^[^\s]+/)[0];
+                returnVars.push(variable);
+            } else if(line.match(/^\s*$/) && returnVars[returnVars.length-1] !== '') {
+                returnVars.push('');
+            }
+        })
+        return returnVars;
+    }
+
 }
